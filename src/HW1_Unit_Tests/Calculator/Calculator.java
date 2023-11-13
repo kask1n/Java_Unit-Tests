49519e64f0f2de950537f3fb5a6d8c08f0efbb50
kask1n <119409700+kask1n@users.noreply.github.com>
2023-11-13 20:00:06 +0300

    HW1: Added function calculatingDiscount

--- src/HW1_Unit_Tests/Calculator/Calculator.java
+++ src/HW1_Unit_Tests/Calculator/Calculator.java
@@ -34,10 +34,10 @@ public class Calculator {
         //  Отрицательные числа
         //  Дробные значения корней
         //  Целые
-            if(num < 0) {
-                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
-            }
-            return Math.sqrt(num);
+        if (num < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(num);
     }
 
     // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
@@ -45,6 +45,10 @@ public class Calculator {
     public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
         // purchaseAmount - сумма покупки
         // discountAmount - размер скидки
-        return 0; // Метод должен возвращать сумму покупки со скидкой
+        if (purchaseAmount < 0 || discountAmount < 0 || discountAmount > 100) {
+            throw new ArithmeticException("invalid arguments");
+        }
+
+        return purchaseAmount - ((double) purchaseAmount * discountAmount / 100); // Метод должен возвращать сумму покупки со скидкой
     }
 }
